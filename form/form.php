683b5cd45433541ffb82f6f1ac914f2fa9a141ef
DerTypda25 <jonas-wigger@hotmail.com>
2023-03-20 18:25:11 +0100

    form styling

--- form/form.php
+++ form/form.php
@@ -150,7 +150,6 @@
 <style>
 
 label{display: block;}
-form{position: relative;}
     .loading{display: none;position: absolute;top: 0;left: 0;width: 100%;height: 100%;background-color: rgba(255, 255, 255, 0.8);z-index: 2;}
     .loading .animation{display: block;position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);z-index: 3;border: 8px solid #f3f3f3;border-top: 8px solid #3498db;border-radius: 50%;width: 50px;height: 50px;animation: spin 1s linear infinite;}
     @keyframes spin {
